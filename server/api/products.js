aeca69ad0d410f9651811c42363edad2ca4793ba
Nida Jabbar <nj2296@columbia.edu>
2019-06-12 18:02:44 -0400

    made models more dry by including order details & adding orderProduct through table

--- server/api/products.js
+++ server/api/products.js
@@ -1,25 +1,24 @@
 const router = require('express').Router()
-const {Product, Item} = require('../db/models')
+const {Product} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    let products = await Product.findAll({
-      include: [{model: Item}]
+    let name = req.query.name.split('-').join(' ')
+    console.log(name)
+    let product = await Product.findAll({
+      where: {name: name}
     })
-    res.json(products)
+    res.status(200).json(product)
   } catch (error) {
     next(error)
   }
 })
 
-router.get('/:id', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
-    let product = await Product.findOne({
-      where: {id: req.params.id},
-      include: [{model: Item}]
-    })
-    res.json(product)
+    let products = await Product.findUniqueStyle()
+    res.status(200).json(products)
   } catch (error) {
     next(error)
   }
