f4a76187af69da6419a9e0ae176de2f4124c5a2a
Nida Jabbar <nj2296@columbia.edu>
2019-06-14 01:01:35 -0400

    updated single page view with add to cart button, need to fix bug

--- client/components/SingleProductDetails.js
+++ client/components/SingleProductDetails.js
@@ -1,19 +1,46 @@
 import React from 'react'
 import {getProductStyleThunk} from '../store/product'
+import {addItemThunk} from '../store/cart'
 import {connect} from 'react-redux'
 
 class SingleProductDetails extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      quantity: ''
+      // size: ''
+    }
+    this.handleChangeQunatity = this.handleChangeQunatity.bind(this)
+    // this.handleChangeSize = this.handleChangeSize.bind(this)
+  }
+
   componentDidMount() {
     let params = new URLSearchParams(document.location.search)
     let productName = params.get('name')
     this.props.getProductStyleThunk(productName)
+    this.props.addItemThunk(
+      this.props.order.id,
+      product.id,
+      this.state.qunatity
+    )
   }
 
+  handleChangeQunatity(event) {
+    event.preventDefault()
+    this.setState({quantity: event.target.value})
+  }
+
+  // handleChangeSize(event) {
+  //   event.preventDefault()
+  //   // this.setState({size: event.target.value})
+  // }
+
   render() {
+    console.log(this)
     if (this.props.loading) {
       return <div>LOADING...</div>
     }
-    let {productStyle} = this.props
+    let {productStyle, order} = this.props
     return productStyle.length < 1 ? (
       ''
     ) : (
@@ -24,7 +51,7 @@ class SingleProductDetails extends React.Component {
         <p>${productStyle[0].price}</p>
         <div>
           <label>Quantity: </label>
-          <select name="Quantity">
+          <select onChange={this.handleChangeQunatity} name="Quantity">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -42,17 +69,30 @@ class SingleProductDetails extends React.Component {
             ))}
           </select>
         </div>
+        <div>
+          <button
+            type="submit"
+            onClick={() =>
+              this.props.addItemThunk(order.id, product.id, this.state.quantity)
+            }
+          >
+            Add To Cart
+          </button>
+        </div>
       </div>
     )
   }
 }
 const mapStateToProps = state => ({
   productStyle: state.product.productStyle,
-  loading: state.product.loading
+  loading: state.product.loading,
+  order: state.cart.order
 })
 
 const mapDispatchToProps = dispatch => ({
-  getProductStyleThunk: name => dispatch(getProductStyleThunk(name))
+  getProductStyleThunk: name => dispatch(getProductStyleThunk(name)),
+  addItemThunk: (orderId, productId, quantity) =>
+    dispatch(addItemThunk(orderId, productId, quantity))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(
