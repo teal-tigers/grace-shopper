d545d7809431f7335c200d0386d21536f1f3cd90
Stephanie Wu <stephanie.siyi.wu@gmail.com>
2019-06-15 14:14:14 -0400

    (feat component/checkout) in progress: redirect guest from cart to sign up

--- server/api/cart.js
+++ server/api/cart.js
@@ -4,6 +4,7 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
+    console.log('user: ', req.user)
     let user = await User.findOne({
       where: {id: req.user.id},
       include: [{model: Order}]
