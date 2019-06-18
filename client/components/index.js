6bba9b605086262dd27983b8fec650f9b18ac312
Stephanie Wu <stephanie.siyi.wu@gmail.com>
2019-06-17 22:54:02 -0400

    (feat coeat) after sign up redirect to /account

--- client/components/index.js
+++ client/components/index.js
@@ -4,11 +4,9 @@
  * having to figure out which file they belong to!
  */
 export {default as Navbar} from './navbar'
-export {default as UserHome} from './user-home'
 export {default as AllProducts} from './AllProducts'
 export {default as SingleProductDetails} from './SingleProductDetails'
 export {default as Cart} from './Cart'
 export {Login, Signup} from './auth-form'
 export {default as UserAccount} from './UserAccount'
 export {default as ThankYou} from './ThankYou'
-export {default as SignupConfirm} from './SignUpConfirm'
